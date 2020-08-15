fdb35eedce57a61abf4ae0b8b27539792f6cd01d
skittles147 <shahbfd@gmail.com>
2020-08-15 11:34:04 +0100

    MasterChange
    MasterChange

---
+++ Unitty project tutorial 1/Assets/LevelController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+    private static int _nextLevelIndex =1;
+    private Enemy[] _enemies;
+
+    private void OnEnable() 
+    {
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Enemy enemy in _enemies)
+        {
+            if (enemy != null)
+            return;
+        }
+
+        Debug.Log ("YOu Killed All Enemies");
+
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
